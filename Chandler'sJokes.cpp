d966dddeedb8b21bc0ad9aa0d4b7cbf59d6798b9
Abrar Ahmed <102857826+abrar2030@users.noreply.github.com>
2022-12-10 11:06:38 +0100

    Create Chandler'sJokes.cpp

---
+++ Chandler'sJokes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+int n, x[100],a=0,b=0, c=0, d=0, e=0, f=0;
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>x[i];
+if (x[i]==0){
+        a++;
+}else if(x[i]==1){
+        b++;
+}else if(x[i]==2){
+        c++;
+}else if(x[i]==3){
+        d++;
+}else if(x[i]==4){
+        e++;
+}else if(x[i]==5){
+       f++;
+}else{}}
+cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<endl;
+ 
+}
